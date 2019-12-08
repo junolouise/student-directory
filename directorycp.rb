9006fd39c4e61ee8310f8cfef255c939d7478cc4
Katie Rodgers <katierodgers95@gmail.com>
2019-12-08 15:33:09 +0000

    added save feature

--- directorycp.rb
+++ directorycp.rb
@@ -31,7 +31,6 @@ end
 
 def print_students_list
   students_by_cohort = {}
-
   @students.each do |student|
     cohort = student[:cohort]
     name = student[:name]
@@ -59,9 +58,11 @@ end
 def process(selection)
   case selection
   when "1"
-    @students = input_students
+    input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit # this will cause the program to terminate
   else
@@ -78,8 +79,22 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 interactive_menu
+save_students
 #nothing happens until we call the methods
